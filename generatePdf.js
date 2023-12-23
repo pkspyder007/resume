77847d1c1697d33b17baeb26609078ba250d4def
pkspyder <pkspyder@Quizizzs-MacBook-Pro.local>
2023-12-23 10:56:55 +0530

    update exp

--- generatePdf.js
+++ generatePdf.js
@@ -2,9 +2,9 @@ const html_to_pdf = require('html-pdf-node');
 const fs = require('fs')
 
 let options = { format: 'A4', scale: 1.15, format: 'A4', pageRanges: '1' };
-let file = { url: "http://localhost:3000/" };
+let file = { url: "http://127.0.0.1:8080/resume.html" };
 html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-    fs.writeFileSync('docs/Praveen_Kumar_resume.pdf', pdfBuffer, {
+    fs.writeFileSync('docs/praveen_kumar.pdf', pdfBuffer, {
         flag: "w+"
     });
     console.log("RESUME GENERATED");
